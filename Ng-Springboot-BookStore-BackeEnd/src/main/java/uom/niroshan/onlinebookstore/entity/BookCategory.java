f88ffb0f4ff61f7782affd32da5ad96d2a0b37e8
niroshan123 <niroshanratnayake07@gmail.com>
2020-05-01 19:55:34 +0530

    Back End Setting up

---
+++ Ng-Springboot-BookStore-BackeEnd/src/main/java/uom/niroshan/onlinebookstore/entity/BookCategory.java
@@ -0,0 +1,29 @@
+package uom.niroshan.onlinebookstore.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="tbl_category")
+@Setter
+@Getter
+@ToString
+public class BookCategory {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="category_name")
+	private String categoryName;
+	
+	@OneToMany(cascade= CascadeType.ALL, mappedBy="category")
+	private Set<Book> book;
+	
+	//Add setters and getters if you are not using lombok
+	
+}
---
+++ Ng-Springboot-BookStore-BackeEnd/src/main/java/uom/niroshan/onlinebookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,11 @@
+package uom.niroshan.onlinebookstore.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import uom.niroshan.onlinebookstore.entity.BookCategory;
+
+//@RepositoryRestResource(collectionResourceRel="bookCateogry", path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
+
+}
